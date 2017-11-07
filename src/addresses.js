82a29258419eee81a4c8c4f138afbe3af84432d1
Sverre Stikbakke <sverrsti@ntnu.no>
2017-11-07 21:59:54 +0100

    Module VEIBLIKK_address

--- src/addresses.js
+++ src/addresses.js
@@ -1,35 +1,60 @@
-var VEIBLIKK = (function () {
+var VEIBLIKK_address = (function () {
 
-var from = null;
-var to = null;
+  var route_points = {
+    'start_x': null,
+    'start_y': null,
+    'destination_x': null,
+    'destination_y': null
+  };
 
-$('#search_button').click(function(){
-  from = $('#from').val();
-  to = $('#to').val();
-  get_startpoint();
-});
+  var start_address = null;
+  var destination_address = null;
 
-var get_startpoint = function () {
-  $.ajax({
-    url: 'http://www.norgeskart.no/ws/adr.py?' + encodeURI(from),
-    success: get_startpoint_success
+  var proj4_25832_to_25833 = function (x, y) {
+    return proj4("EPSG:25832", "EPSG:25833", [x, y]);
+  };
+
+  var parse_address_JSON = function (address_JSON) {
+    var address = $.parseJSON(address_JSON)[0];
+    var address_x = parseFloat(address["LONGITUDE"]);
+    var address_y = parseFloat(address["LATITUDE"]);
+    var address_point = proj4_25832_to_25833(address_x, address_y);
+    return address_point;
+  }
+
+  $('#search_button').click(function () {
+    start_address = $('#start_address').val();
+    destination_address = $('#destination_address').val();
+    get_starting_point();
   });
-};
 
-var get_startpoint_success = function (start_address) {
-  console.log(start_address);
-  get_endpoint();
-}
+  var get_starting_point = function () {
+    $.ajax({
+      url: 'http://www.norgeskart.no/ws/adr.py?' + encodeURI(start_address),
+      success: get_starting_point_success
+    });
+  };
 
-var get_endpoint = function () {
-  $.ajax({
-    url: 'http://www.norgeskart.no/ws/adr.py?' + encodeURI(to),
-    success: get_endpoint_success
-  });
-};
+  var get_starting_point_success = function (start_address_JSON) {
+    var start_point = parse_address_JSON(start_address_JSON);
+    route_points['start_x'] = start_point[0];
+    route_points['start_y'] = start_point[1];
+    get_destination_point();
+  }
 
-var get_endpoint_success = function (destination_address) {
-  console.log(destination_address);
-}
+  var get_destination_point = function () {
+    $.ajax({
+      url: 'http://www.norgeskart.no/ws/adr.py?' + encodeURI(destination_address),
+      success: get_destination_point_success
+    });
+  };
+
+  var get_destination_point_success = function (destination_address_JSON) {
+    var destination_point = parse_address_JSON(destination_address_JSON);
+    route_points['destination_x'] = destination_point[0];
+    route_points['destination_y'] = destination_point[1];
+  }
+
+  return { route_points };
 
 }())
