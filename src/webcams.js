15be37e370b9151a2a1cc8a7d4505c7ae203f636
Sverre Stikbakke <sverre.stikbakke@gmail.com>
2017-12-30 15:09:03 +0100

    Refactor: store_cctv_point function, changed function order and some names.

--- src/webcams.js
+++ src/webcams.js
@@ -38,12 +38,20 @@ var VEIBLIKK_webcams = (function () {
     units: 'kilometers'
   };
 
-  var segment_length = 30;
-  var buffer_width = 50;
+  var segment_length = 30; // kilometers
+  var buffer_width = 50; // meters
 
 
-  var preprocess_cctv_locations = function (xhr) {
+  var preprocess_cctv_records = function (xhr) {
     cctv_JSON = parser.parse(xhr.response);
+  };
+
+
+  var get_cctv_file_error = function (error) {
+    VEIBLIKK_messages.ux_message(
+      '#status_message',
+      'Får ikke hentet webkamera-info. ' + error,
+      'error');
   };
 
 
@@ -54,14 +62,17 @@ var VEIBLIKK_webcams = (function () {
 
 
   var make_segments = function () {
+
+    t0 = performance.now();
+
     segment_index = 0;
     cctv_locations_route = [];
 
     /** 
-   * Split route in short segments to increase
-   * performance in PointInPolygon function.
-   * 30 km segments may be a sweet spot.
-   */
+     * Split route in short segments to increase
+     * performance in PointInPolygon function.
+     * 30 km segments may be a sweet spot.
+     */
 
     route_segments = turf.lineChunk(
       route,
@@ -69,87 +80,36 @@ var VEIBLIKK_webcams = (function () {
       option_units_kilometers
     );
 
-    t0 = performance.now();
-
-    setTimeout(cctv_segment_loop(), 0);
+    setTimeout(route_segments_loop(), 0);
   };
 
 
-  var cctv_segment_loop = function () {
-    route_segment = route_segments.features[segment_index];
-    segment_index++;
-
-    if (segment_index > route_segments.features.length) {
-      setTimeout(cctv_display, 0);
-      return true;
-    };
-
-    setTimeout(cctvs_in_segment, 0);
-  };
-
-
-  var cctvs_in_segment = function () {
-    var route_buffer = turf.buffer(
-      route_segment,
-      buffer_width,
-      option_units_meters
+  var store_cctv_point = function (cctv_point, cctv_record) {
+    var cctv_snapped = turf.nearestPointOnLine(
+      route,
+      cctv_point,
+      option_units_kilometers
     );
 
-    Bliss.each(cctv_JSON
-      .d2LogicalModel
-      .payloadPublication
-      .genericPublicationExtension
-      .cctvSiteTablePublication
-      .cctvCameraList
-      .cctvCameraMetadataRecord,
-      function (index, cctv_location) {
+    cctv_snapped['properties']['stillImageUrl'] = cctv_record
+      .cctvStillImageService
+      .stillImageUrl
+      .urlLinkAddress;
 
-        var cctv_lon = parseFloat(cctv_location
-          .cctvCameraLocation
-          .pointByCoordinates
-          .pointCoordinates.longitude
-        );
+    cctv_snapped['properties']['urlLinkDescription'] = cctv_record
+      .cctvStillImageService
+      .stillImageUrl
+      .urlLinkDescription
+      .values
+      .value;
 
-        var cctv_lat = parseFloat(cctv_location
-          .cctvCameraLocation
-          .pointByCoordinates
-          .pointCoordinates.latitude
-        );
+    cctv_snapped['properties']['cctvCameraSite'] = cctv_record
+      .cctvCameraSiteLocalDescription
+      .values
+      .value;
 
-        var cctv_point = turf.point([cctv_lon, cctv_lat]);
-
-        if (turf.booleanPointInPolygon(cctv_point, route_buffer)) {
-
-          var cctv_snapped = turf.nearestPointOnLine(
-            route,
-            cctv_point,
-            option_units_kilometers
-          );
-
-          cctv_snapped['properties']['stillImageUrl'] = cctv_location
-            .cctvStillImageService
-            .stillImageUrl
-            .urlLinkAddress;
-
-          cctv_snapped['properties']['urlLinkDescription'] = cctv_location
-            .cctvStillImageService
-            .stillImageUrl
-            .urlLinkDescription
-            .values
-            .value;
-
-          cctv_snapped['properties']['cctvCameraSite'] = cctv_location
-            .cctvCameraSiteLocalDescription
-            .values
-            .value;
-
-          cctv_locations_route.push(cctv_snapped);
-        };
-      }
-    );
-    setTimeout(cctv_segment_loop, 0);
+    cctv_locations_route.push(cctv_snapped);
   };
-
 
   var cctv_display = function () {
 
@@ -198,17 +158,61 @@ var VEIBLIKK_webcams = (function () {
   };
 
 
-  var get_cctvs_file_error = function (error) {
-    VEIBLIKK_messages.ux_message(
-      '#status_message',
-      'Får ikke hentet webkamera-info. ' + error,
-      'error');
+  var cctvs_in_segment = function () {
+    var route_buffer = turf.buffer(
+      route_segment,
+      buffer_width,
+      option_units_meters
+    );
+
+    Bliss.each(cctv_JSON
+      .d2LogicalModel
+      .payloadPublication
+      .genericPublicationExtension
+      .cctvSiteTablePublication
+      .cctvCameraList
+      .cctvCameraMetadataRecord,
+      function (index, cctv_record) {
+
+        var cctv_lon = parseFloat(cctv_record
+          .cctvCameraLocation
+          .pointByCoordinates
+          .pointCoordinates.longitude
+        );
+
+        var cctv_lat = parseFloat(cctv_record
+          .cctvCameraLocation
+          .pointByCoordinates
+          .pointCoordinates.latitude
+        );
+
+        var cctv_point = turf.point([cctv_lon, cctv_lat]);
+
+        if (turf.booleanPointInPolygon(cctv_point, route_buffer)) {
+          store_cctv_point(cctv_point, cctv_record);
+        };
+      }
+    );
+    setTimeout(route_segments_loop, 0);
+  };
+
+
+  var route_segments_loop = function () {
+    route_segment = route_segments.features[segment_index];
+    segment_index++;
+
+    if (segment_index > route_segments.features.length) {
+      setTimeout(cctv_display, 0);
+      return true;
+    };
+
+    setTimeout(cctvs_in_segment, 0);
   };
 
 
   Bliss.fetch('GetCCTVSiteTable.xml')
-    .then(preprocess_cctv_locations)
-    .catch(get_cctvs_file_error);
+    .then(preprocess_cctv_records)
+    .catch(get_cctv_file_error);
 
 
   return {
